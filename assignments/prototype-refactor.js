d3dcc6d938f6a42bba5a84b5eb6d4c891eecbf6c
fiopwk <flopwiki@gmail.com>
2019-09-19 17:54:39 -0400

    copied JS III to prototype

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -4,6 +4,136 @@ Prototype Refactor
 
 1. Copy and paste your code or the solution from yesterday
 
+/*
+  === GameObject ===
+  * createdAt
+  * name
+  * dimensions (These represent the character's size in the video game)
+  * destroy() // prototype method that returns: `${this.name} was removed from the game.`
+*/
+function GameObject(attrs) {
+    this.createdAt = attrs.createdAt
+  
+    this.name = attrs.name,
+    this.dimensions = function(length, width, height) {
+      return (attrs.length, attrs.width, attrs.height);
+    }
+  
+    this.destroy = function(name) {
+      return `${name} was removed from the game.`;
+    }
+  
+  }
+  /*
+    === CharacterStats ===
+    * healthPoints
+    * takeDamage() // prototype method -> returns the string '<object name> took damage.'
+    * should inherit destroy() from GameObject's prototype
+  */
+  function CharacterStats(object) {
+    this.healthPoints = object.healthPoints,
+    this.takeDamage = () => {
+      return `${object.name} took damage.`;
+  
+    }
+  
+    destroy.prototype = function(object) {
+      return `${this.name} was removed from the game.`;
+    }
+  
+  }
+  /*
+    === Humanoid (Having an appearance or character resembling that of a human.) ===
+    * team
+    * weapons
+    * language
+    * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
+    * should inherit destroy() from GameObject through CharacterStats
+    * should inherit takeDamage() from CharacterStats
+  */
+   function Humanoid(attrs) {
+    this.team = attrs.team,
+    this.weapons = attrs.weapons,
+    this.language = attrs.language,
+  
+    this.greet = function() {
+      return (`${attrs.name} offers a greeting in ${attrs.language}`);
+    }
+  
+  
+  
+   }
+  /*
+    * Inheritance chain: GameObject -> CharacterStats -> Humanoid
+    * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
+    * Instances of CharacterStats should have all of the same properties as GameObject.
+  */
+  
+  // Test you work by un-commenting these 3 objects and the list of console logs below:
+  
+  
+    const mage = new Humanoid({
+      createdAt: new Date(),
+      dimensions: {
+        length: 2,
+        width: 1,
+        height: 1,
+      },
+      healthPoints: 5,
+      name: 'Bruce',
+      team: 'Mage Guild',
+      weapons: [
+        'Staff of Shamalama',
+      ],
+      language: 'Common Tongue',
+    });
+  
+    const swordsman = new Humanoid({
+      createdAt: new Date(),
+      dimensions: {
+        length: 2,
+        width: 2,
+        height: 2,
+      },
+      healthPoints: 15,
+      name: 'Sir Mustachio',
+      team: 'The Round Table',
+      weapons: [
+        'Giant Sword',
+        'Shield',
+      ],
+      language: 'Common Tongue',
+    });
+  
+    const archer = new Humanoid({
+      createdAt: new Date(),
+      dimensions: {
+        length: 1,
+        width: 2,
+        height: 4,
+      },
+      healthPoints: 10,
+      name: 'Lilith',
+      team: 'Forest Kingdom',
+      weapons: [
+        'Bow',
+        'Dagger',
+      ],
+      language: 'Elvish',
+    });
+  
+    console.log(mage.createdAt); // Today's date
+    console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+    console.log(swordsman.healthPoints); // 15
+    console.log(mage.name); // Bruce
+    console.log(swordsman.team); // The Round Table <=
+    console.log(mage.weapons); // Staff of Shamalama <=
+    console.log(archer.language); // Elvish <=
+    console.log(archer.greet()); // Lilith offers a greeting in Elvish. <= 
+    console.log(mage.takeDamage()); // Bruce took damage.
+    console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+
+
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
 */
